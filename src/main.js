30ee37bcf2394c93dfe1240a117494a5e6a97580
crows <944303139@qq.com>
2020-10-27 11:26:34 +0800

    vant组件导入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
 // 全局样式
 import './styles/index.less'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
